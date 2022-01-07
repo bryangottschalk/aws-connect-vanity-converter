735cf9e1c63ae0bd027337660c5a52f7cf9a2262
Bryan Gottschalk <bgottschalk@hntb.com>
2022-01-07 14:42:39 -0700

    define dynamodb table and vanity converter function

---
+++ sam/src/handlers/vanity-phone-number-converter.ts
@@ -0,0 +1,9 @@
+exports.handler = async () => {
+  // If you change this message, you will need to change hello-from-lambda.test.js
+  const message = "TODO: add vanity phone number converter function";
+
+  // All log statements are written to CloudWatch
+  console.info(`${message}`);
+
+  return message;
+};
