a868d3cd340bcb8946389d7196260142daa015db
Bryan Gottschalk <bgottschalk@hntb.com>
2022-01-08 20:28:25 -0700

    add response logic for contact flow prompt based on results

--- sam/src/handlers/vanity-phone-number-converter.ts
+++ sam/src/handlers/vanity-phone-number-converter.ts
@@ -1,5 +1,5 @@
-import { DynamoDB } from "aws-sdk";
-const words: string[] = require("an-array-of-english-words");
+import { DynamoDB } from 'aws-sdk';
+const words: string[] = require('an-array-of-english-words');
 const DDB = new DynamoDB.DocumentClient();
 interface PhoneCharMap {
   [key: string]: string[];
@@ -8,18 +8,18 @@ const phoneCharacterMap: PhoneCharMap = {
   // Assume 0 and 1 are not in the phone number for now as they aren't on the keypad
   // '0': [""],
   // '1': [""],
-  "2": ["a", "b", "c"],
-  "3": ["d", "e", "f"],
-  "4": ["g", "h", "i"],
-  "5": ["j", "k", "l"],
-  "6": ["m", "n", "o"],
-  "7": ["p", "q", "r", "s"],
-  "8": ["t", "u", "v"],
-  "9": ["w", "x", "y", "z"],
+  '2': ['a', 'b', 'c'],
+  '3': ['d', 'e', 'f'],
+  '4': ['g', 'h', 'i'],
+  '5': ['j', 'k', 'l'],
+  '6': ['m', 'n', 'o'],
+  '7': ['p', 'q', 'r', 's'],
+  '8': ['t', 'u', 'v'],
+  '9': ['w', 'x', 'y', 'z']
 };
 
 const getPermutationsIterative = (phoneNumber: string): string[] => {
-  let queue = [""];
+  let queue = [''];
   /*
    phone number = 800-474-2253 example 
    1st iteration - queue is [""]
@@ -41,9 +41,36 @@ const getPermutationsIterative = (phoneNumber: string): string[] => {
   return queue;
 };
 
-export const handler = async (event) => {
+const replaceUnhandledDigits = (callerPhoneNumber: string) => {
+  let res = '';
+  callerPhoneNumber.split('').forEach((digit) => {
+    if (digit === '0' || digit === '1') {
+      /*
+      since 0 and 1 have no values to map to on dialpads, hard code to 2.
+      in production would likely return 0 results, an error, or check a different 
+      portion of the number for vanity possibilities instead.
+      */
+      res += '2';
+    } else {
+      res += digit;
+    }
+  });
+  return res;
+};
+
+export const handler = async (event, context, callback) => {
+  let callerPhoneNumber = '';
+  if (event['Details']) {
+    callerPhoneNumber =
+      event['Details']['ContactData']['CustomerEndpoint']['Address'].slice(2); // strip country code
+  } else {
+    callerPhoneNumber = '8004742253';
+  }
+
+  const formattedPhoneNumber = replaceUnhandledDigits(callerPhoneNumber);
+  console.log('formattedPhoneNumber', formattedPhoneNumber);
   /*
-  8004742253 example returns:
+  8004742253 fallback example for local testing returns:
   vanityNumbers [
   '800GRIBBLE',
   '800474ABLE',
@@ -52,18 +79,17 @@ export const handler = async (event) => {
   '800474BALE'
   ]
   */
-  const phoneNumber = "8004742253"; // TODO: get dynamically from connect
-  if (phoneNumber.length < 10) {
+  if (formattedPhoneNumber.length < 10) {
     return [];
   }
-  const first3 = phoneNumber.slice(0, 3);
-  const last7 = phoneNumber.slice(phoneNumber.length - 7);
-  const last4 = phoneNumber.slice(phoneNumber.length - 4);
-  const first6 = phoneNumber.slice(0, 6);
+  const first3 = formattedPhoneNumber.slice(0, 3);
+  const last7 = formattedPhoneNumber.slice(formattedPhoneNumber.length - 7);
+  const last4 = formattedPhoneNumber.slice(formattedPhoneNumber.length - 4);
+  const first6 = formattedPhoneNumber.slice(0, 6);
 
   // ideally format is 3DIGITAREACODE-LAST7 - check for that first
   let permutations = getPermutationsIterative(last7);
-  console.log("7 letter permutations:", permutations);
+  console.log('7 letter permutations:', permutations);
 
   const vanityNumbers: string[] = [];
   for (let i = 0; i < permutations.length; i++) {
@@ -81,7 +107,7 @@ export const handler = async (event) => {
   if (vanityNumbers.length < 5) {
     // couldn't get 5 results with single words; run again against last4 for higher chance of results
     permutations = getPermutationsIterative(last4);
-    console.log("4 letter permutations:", permutations);
+    console.log('4 letter permutations:', permutations);
     for (let i = 0; i < permutations.length; i++) {
       let permutation = permutations[i];
       if (vanityNumbers.length >= 5) {
@@ -94,23 +120,59 @@ export const handler = async (event) => {
       }
     }
   }
-  console.log("vanityNumbers", vanityNumbers);
-  await saveToDB(phoneNumber, vanityNumbers);
-  return vanityNumbers;
+  console.log('vanityNumbers', vanityNumbers);
+  await saveToDB(callerPhoneNumber, formattedPhoneNumber, vanityNumbers);
+  // return vanityNumbers;
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      vanityNumbers,
+      callerPhoneNumber
+    })
+  };
+  console.log('response:', response);
+  let connectBotResponse;
+  if (vanityNumbers.length) {
+    const vanityNumbersJoined = vanityNumbers.join(', '); // so the bot can read the vanity number conversationally
+    const csvVanityNumbers = vanityNumbers.join('').split('').join(', '); // so the bot can spell out the digits more slowly
+    const csvCallerPhoneNumber = callerPhoneNumber.split('').join(', '); // so the bot can spell out the digits more slowly
+    connectBotResponse = `Thank you for calling our vanity number generator. 
+    We have successfully saved vanity phone numbers based on your caller number to our DynamoDB Database. 
+    Your phone number is ${csvCallerPhoneNumber}. The vanity numbers generated for this are: ${vanityNumbersJoined}. 
+    Spelled out they are ${csvVanityNumbers}...`;
+    if (callerPhoneNumber.includes('0') || callerPhoneNumber.includes('1')) {
+      connectBotResponse +=
+        "Please note that because your phone number includes a zero or a one in it, those numbers were replaced with 2's in your vanity numbers so they can correspond with a phone keypad...";
+    }
+  } else {
+    connectBotResponse =
+      'Thank you for calling our vanity number generator. Unfortunately, your phone number did not generate any results using the words we have available to compare to...';
+  }
+  connectBotResponse += 'Thanks for using our services. Have a great day!`';
+  callback(null, {
+    connectBotResponse
+  });
+  return response;
 };
 
-const saveToDB = async (phoneNumber: string, vanityNumbers: string[]) => {
+const saveToDB = async (
+  callerPhoneNumber: string,
+  formattedPhoneNumber: string,
+  vanityNumbers: string[]
+) => {
   try {
     await DDB.put({
       TableName: process.env.TABLE_NAME,
       Item: {
-        callerPhoneNumber: phoneNumber,
-        vanityNumbers,
+        callerPhoneNumber,
+        formattedPhoneNumber,
+        vanityNumbers
       },
+      ConditionExpression: 'attribute_not_exists(pk)'
     }).promise();
-    console.log("save to DDB success");
+    console.log('save to DDB success');
   } catch (err) {
-    console.log("save to DDB error:");
+    console.log('save to DDB error:');
     console.info(err);
   }
 };
--- sam/tests/unit/vanity-phone-number-converter.spec.ts
+++ sam/tests/unit/vanity-phone-number-converter.spec.ts
@@ -1,7 +1,7 @@
-import { expect } from "chai";
-import { handler as vanityPhoneNumberConverterHandler } from "../../src/handlers/vanity-phone-number-converter";
-import * as AWS from "aws-sdk";
-import * as AWSMock from "aws-sdk-mock";
+import { expect } from 'chai';
+import { handler as vanityPhoneNumberConverterHandler } from '../../src/handlers/vanity-phone-number-converter';
+import * as AWS from 'aws-sdk';
+import * as AWSMock from 'aws-sdk-mock';
 
 /*
 Test plans
@@ -13,11 +13,18 @@ Test plans
 - handle case of duplicate record (caller phone number has already invoked lambda in the past)
 */
 
-describe("Test for vanity-phone-number-converter", function () {
-  it("Verifies successful response", async () => {
-    const result: string[] = await vanityPhoneNumberConverterHandler({
-      phoneNumber: "8004742253",
-    });
-    expect(result).to.be.an("array");
+describe('Test for vanity-phone-number-converter', function () {
+  it('Verifies successful response', async () => {
+    const result: any = await vanityPhoneNumberConverterHandler(
+      {
+        phoneNumber: '8004742253'
+      },
+      null,
+      null
+    );
+    // const result: string[] = await vanityPhoneNumberConverterHandler({
+    //   phoneNumber: "8004742253",
+    // });
+    expect(result).to.be.an('array');
   });
 });
